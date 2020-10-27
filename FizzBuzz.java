3f42dbac3c219a786e27b8be59fb2c044a9e341f
NIKHILTHADESHWAR <nikhilthadeshwar2.com>
2020-10-28 00:56:30 +0530

    Refactor for n=15(when multiple of 3 and 5)

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -15,7 +15,9 @@ public class FizzBuzz {
     }
 
     public static String generateFizzBuzz(int num) {
-        if(num%3 == 0)
+        if(num%3 == 0 && num%5 == 0)
+            return "FizzBuzz";
+        else if(num%3 == 0)
             return "Fizz";
         else if(num%5 == 0)
             return "Buzz";
