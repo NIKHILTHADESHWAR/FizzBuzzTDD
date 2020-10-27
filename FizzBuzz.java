03e78f8983a5f79b7b3edae5ccccb0b16fbe69d4
NIKHILTHADESHWAR <nikhilthadeshwar2.com>
2020-10-28 00:47:26 +0530

    Test for n=6(multiple of 3) - Failed

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -25,6 +25,6 @@ public class FizzBuzz {
 
     @Test
     public void testFizzBuzz() {
-        assertEquals("Buzz",generateFizzBuzz(5));
+        assertEquals("Fizz",generateFizzBuzz(6));
     }
 }
