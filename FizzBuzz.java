a832880f17907d7a38660526b2dcdfdf1a680041
NIKHILTHADESHWAR <nikhilthadeshwar2.com>
2020-10-28 00:51:33 +0530

    Refactor for n=10(when multilple of 5)

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -17,7 +17,7 @@ public class FizzBuzz {
     public static String generateFizzBuzz(int num) {
         if(num%3 == 0)
             return "Fizz";
-        else if(num==5)
+        else if(num%5 == 0)
             return "Buzz";
         else
             return Integer.toString(num);
